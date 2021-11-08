0355e4b2f340a8cbb72a5a3b13395f0529279383
AlterGeist <dariodrame73@gmail.com>
2021-11-08 16:12:09 +0100

    Changes?
    Neki update valjda

--- AllAboard/Assets/Code/SomeScript.cs
+++ AllAboard/Assets/Code/SomeScript.cs
@@ -1,18 +1,34 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class SomeScript : MonoBehaviour
 {
     public GameObject panelToClose;
     public GameObject mainMenu;
+    public List<RectTransform> buttons;
+    public RectTransform continueText;
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && panelToClose.gameObject.activeSelf==true)
         {
+            LeanTween.scale(panelToClose, Vector3.zero, 0.5f).setEaseInBack();
             panelToClose.gameObject.SetActive(false);
+            mainMenu.transform.localScale = Vector3.zero;
             mainMenu.gameObject.SetActive(true);
+            LeanTween.scale(mainMenu, Vector3.one, 1f).setEaseOutCubic().setDelay(0.5f);
+
+            foreach (RectTransform b in buttons)
+            {
+                LeanTween.alpha(b, 1f, 3f);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1) && panelToClose.gameObject.activeSelf == false)
+        {     
+            mainMenu.gameObject.SetActive(false);
+            panelToClose.gameObject.SetActive(true);
+            LeanTween.scale(panelToClose, Vector3.one, 0.5f);
+            LeanTween.alpha(continueText, 0f, 1f).setLoopPingPong(-1);
         }
     }
 }
