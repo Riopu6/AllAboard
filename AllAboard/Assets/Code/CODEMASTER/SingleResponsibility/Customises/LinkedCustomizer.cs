43869f0c7a7b1275fcf48501d0729a1be968bec5
Riopu6 <vale.riopu6@gmail.com>
2022-03-03 20:52:45 +0100

    3.3.22 Update

---
+++ AllAboard/Assets/Code/CODEMASTER/SingleResponsibility/Customises/LinkedCustomizer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class LinkedCustomizer : Customizer
+{
+	public List<GameObject> ColorLinkedObjects;
+
+	public override void Colorize()
+	{
+		base.Colorize();
+
+		var linkedColor = ColorLinkedObjects[0].GetComponent<Renderer>().material.color;
+		foreach (var item in ColorLinkedObjects)
+		{
+			item.GetComponent<Renderer>().material.color = linkedColor;
+		}
+	}
+}
